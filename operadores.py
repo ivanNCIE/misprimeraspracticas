4771ac069ae0e7f35ecb66c9e0b48b8c186a54f7
ivan <ivan.nodos3k@gmail.com>
2019-03-12 18:05:20 -0600

    primer cambio
    primer cambio de mi programa

---
+++ operadores.py
@@ -0,0 +1,10 @@
+# programa que realiza una operacion aritmetica
+operacion = 1-5*6+20 # -9
+operacion2 = 4       # 4
+
+operacion = "2"
+operacion = "2"
+resultado = operacion + operacion2
+
+# -9+4 = -5
+print(resultado)
